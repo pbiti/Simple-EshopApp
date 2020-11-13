95a5b49585dc3250a5b7b1df369ade1efb208d41
pbiti <72506968+pbiti@users.noreply.github.com>
2020-11-13 16:05:02 +0200

    Delete mysqldb.sql

--- project/mysqldb.sql
+++
@@ -1,8 +0,0 @@
-create database products;
-use products;
-create table if not exists items(
-barcode(100) not null,
-name(100) not null,
-color(100) not null,
-description(300) not null
-primary key(barcode));
